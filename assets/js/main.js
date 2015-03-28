5a4bfd3f0c754445c79ce07f43c3daacfb8b1c19
Brian Tully <brian@Brians-MBP.home>
2015-03-28 11:30:50 -0400

    modal

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){        
+	$('a .thumbnail').on('click',function(){
+		var src = $(this).attr('src');
+		var img = '<img src="' + src + '" class="img-responsive"/>';
+		
+		//start of new code new code
+		var index = $(this).parent('li').index();   
+		
+		var html = '';
+		html += img;                
+		
+		$('#myModal').modal();
+		$('#myModal').on('shown.bs.modal', function(){
+			$('#myModal .modal-body').html(html);
+		})
+		$('#myModal').on('hidden.bs.modal', function(){
+			$('#myModal .modal-body').html('');
+		});
+		
+		
+   });	
+})
